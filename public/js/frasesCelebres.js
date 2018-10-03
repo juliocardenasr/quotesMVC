87cd184f882c3ff9c2095a53ed56640806574f68
panetton <juliocardenasr@gmail.com>
2018-10-03 17:38:09 -0500

    Se tienen todos los elementos del front enlazados y funcionando, la funcion en el javascript es un muck

---
+++ public/js/frasesCelebres.js
@@ -0,0 +1,4 @@
+function traerFrase() {
+    document.getElementById('frase').innerHTML = 'Se traera la frase del dia'
+    document.getElementById('autor').innerHTML = 'aqui ira el autor de la frase'
+}
