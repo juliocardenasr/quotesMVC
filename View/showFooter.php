37a3b3fac5c60ffc5963aae60bd56d673c7d0c0b
panetton <juliocardenasr@gmail.com>
2019-03-12 18:43:36 -0500

    Se modifico la interfase, se llama los archivos html a partes, se implemento el grid en css

---
+++ View/showFooter.php
@@ -0,0 +1,8 @@
+      <div class="footer">
+        <footer>
+          <h3>Pagina desarrollada por Julio Cardenas</h3>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ View/showHeader.php
@@ -0,0 +1,29 @@
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="ico" href="favicon.ico">
+        <link rel="stylesheet" href="css/frasesCelebres.css">
+        <title><?php echo $title; ?></title>
+      </head>
+      <body>
+        <div class="grid">
+          <div class="title">
+            <hgroup>
+              <h1>Pannetoncito</h1>
+              <h2>Sitio4 Frase del Dia</h2>
+            </hgroup>
+          </div>
+          <div class="header">
+            <header>            
+              <img src="img/panneton.jpg" alt="pannetoncito logo" >
+              <nav>
+                <ul>
+                  <li><a href=#>Frases</a></li>
+                  <li><a href=#>Usuarios</a></li>
+                </ul>
+              </nav>
+            </header>
+          </div>
